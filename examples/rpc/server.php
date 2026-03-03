5a384f5e90785a9d42e3d8a0a03113a2bc82b46d
Marcel Gleis <marcel@gleis.de>
2026-03-03 14:47:11 +0100

    empty spaces

--- examples/rpc/server.php
+++ examples/rpc/server.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Simple HTTP server script for KeyValueStoreServer
- * 
+ *
  * Usage: php -S localhost:8080 server.php
  * Then access endpoints like: POST http://localhost:8080/rpc/get
  */
